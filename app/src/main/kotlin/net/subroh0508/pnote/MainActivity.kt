ad82d43c52cf1e6aa31c4bccec2b4e1b6a3362b7
subroh0508 <subroh.0508@gmail.com>
2018-10-25 23:07:34 +0900

    initial commit

---
+++ app/src/main/kotlin/net/subroh0508/pnote/MainActivity.kt
@@ -0,0 +1,19 @@
+package net.subroh0508.pnote
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import net.subroh0508.pnote.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
---
+++ app/src/main/kotlin/net/subroh0508/pnote/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package net.subroh0508.pnote.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
